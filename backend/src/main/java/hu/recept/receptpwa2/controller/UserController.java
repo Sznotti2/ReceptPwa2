7b99519b28d5293d027ce699dd8de8179fa1471a
Kovács János András <kovacs.janos212@gmail.com>
2025-07-25 20:19:45 +0200

    Registration backend v1.

--- backend/src/main/java/hu/recept/receptpwa2/controller/UserController.java
+++ backend/src/main/java/hu/recept/receptpwa2/controller/UserController.java
@@ -3,6 +3,8 @@ package hu.recept.receptpwa2.controller;
 import hu.recept.receptpwa2.model.User;
 import hu.recept.receptpwa2.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -15,12 +17,21 @@ public class UserController {
 
     @PostMapping("/register")
     public User register(@RequestBody User user) {
-        return userRepository.save(user);
+        try {
+            return userRepository.saveAndFlush(user);
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
     }
 
     @PostMapping("/login")
     public User login(@RequestBody User user) {
         return userRepository.findByEmail(user.getEmail());
+    }
+
+    @ExceptionHandler(RuntimeException.class)
+    public ResponseEntity<String> handleCustomException(RuntimeException e) {
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(e.getMessage());
     }
 
 }
